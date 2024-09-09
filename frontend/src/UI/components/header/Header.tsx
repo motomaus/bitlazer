9f5171a86a37e52e18cfa82db42483b774d10613
olehmolchanov <ekkeeyy@gmail.com>
2024-09-09 20:09:15 +0300

    add modals

--- frontend/src/UI/components/header/Header.tsx
+++ frontend/src/UI/components/header/Header.tsx
@@ -1,12 +1,23 @@
-import React, { FC, useState } from 'react'
+import React, { FC, useState, useEffect } from 'react'
 import logo from '../../../assets/images/logowhite.svg'
 import { Link, useLocation } from 'react-router-dom'
 import Button from '@components/button/Button'
+import MyModal from '@components/modal/MyModal'
+import HowItWorks from '@pages/how-it-works/HowItWorks'
+import ConnectWallet from '@pages/connect-wallet/ConnectWallet'
+import Roadmap from '@pages/roadmap/Roadmap'
+import Features from '@pages/features/Features'
+import clsx from 'clsx'
 
-interface IHeader { }
+interface IHeader {}
 
 const Header: FC<IHeader> = () => {
   const [isActive, setIsActive] = useState(false)
+  const [openHowItWorksModal, setOpenHowItWorksModal] = useState(false)
+  const [openConnectWalletModal, setOpenConnectWalletModal] = useState(false)
+  const [openRoadmapModal, setOpenRoadmapModal] = useState(false)
+  const [openFeaturesModal, setOpenFeaturesModal] = useState(false)
+
   const location = useLocation()
 
   const toggleMenu = () => {
@@ -17,70 +28,162 @@ const Header: FC<IHeader> = () => {
     setIsActive(false)
   }
 
-  const navLinks = [
-    { path: '/about', label: '[ABOUT]' },
-    { path: '/bridge', label: '[BRIDGE]' },
-    { path: '/how-it-works', label: '[HOW IT WORKS]' },
-    { path: '/features', label: '[FEATURES]' },
-    { path: '/roadmap', label: '[ROADMAP]' },
-    { path: '/faq', label: '[FAQ]' },
-  ]
+  // Close modals when the location changes
+  useEffect(() => {
+    setOpenHowItWorksModal(false)
+    setOpenConnectWalletModal(false)
+    setOpenRoadmapModal(false)
+    setOpenFeaturesModal(false)
+  }, [location])
 
   return (
-    <header className="w-full fixed md:absolute top-0 left-0 z-50 bg-black md:bg-transparent">
-      <div className="container">
-        <div className="flex flex-row items-center justify-between gap-4">
-          <Link
-            to={'/'}
-            className="h-[5.625rem] w-[9.606rem] flex items-center justify-center flex-shrink-0"
-          >
-            <img className="w-full h-full object-contain" loading="lazy" alt="" src={logo} />
-          </Link>
-          <button
-            id="burger"
-            onClick={toggleMenu}
-            className="w-7 h-7 flex-col justify-center items-center gap-[.3125rem] inline-flex flex-shrink-0 block md:hidden ml-auto"
-          >
-            <span className="w-7 h-0.5 bg-lightgreen-100 rounded-[.1875rem]"></span>
-            <span className="w-7 h-0.5 bg-lightgreen-100 rounded-[.1875rem]"></span>
-            <span className="w-7 h-0.5 bg-lightgreen-100 rounded-[.1875rem]"></span>
-          </button>
-          <div
-            className={`flex h-screen flex-col text-white fixed transition-all md:p-0 px-4 py-8 pt-24 duration-300 z-[100] bg-black w-full top-0 overflow-y-auto md:overflow-visible md:top-auto md:w-auto md:h-auto md:bg-transparent md:static md:z-auto ${isActive ? 'right-0' : '-right-[100vw]'
-              }`}
-          >
-            <button
-              onClick={closeMenu}
-              className="md:hidden absolute top-4 right-4 z-10 h-[2.025rem] w-[1.881rem] text-lightgreen-100 hover:text-lightgreen-100 rounded-[.115rem] bg-darkolivegreen-200 flex flex-col items-center justify-center transition-all duration-300 hover:shadow-[1.8px_1.8px_1.84px_1.4px_rgba(0,_0,_0,_0.91)_inset]"
+    <>
+      <header className="w-full fixed md:absolute top-0 left-0 z-50 bg-black md:bg-transparent">
+        <div className="container">
+          <div className="flex flex-row items-center justify-between gap-4">
+            <Link
+              to={'/'}
+              className="h-[5.625rem] w-[9.606rem] flex items-center justify-center flex-shrink-0"
             >
-              X
+              <img className="w-full h-full object-contain" loading="lazy" alt="" src={logo} />
+            </Link>
+            <button
+              id="burger"
+              onClick={toggleMenu}
+              className="w-7 h-7 flex-col justify-center items-center gap-[.3125rem] inline-flex flex-shrink-0 block md:hidden ml-auto"
+            >
+              <span className="w-7 h-0.5 bg-lightgreen-100 rounded-[.1875rem]"></span>
+              <span className="w-7 h-0.5 bg-lightgreen-100 rounded-[.1875rem]"></span>
+              <span className="w-7 h-0.5 bg-lightgreen-100 rounded-[.1875rem]"></span>
             </button>
-            <div className="items-center gap-[4.875rem] flex md:flex-row flex-col">
-              <nav className="whitespace-nowrap text-[1.25rem] text-lightgreen-100 font-ocr-x-trial">
-                <ul className="flex md:flex-row flex-col items-center gap-8">
-                  {navLinks.map((link, index) => (
-                    <li key={index}>
+            <div
+              className={`flex h-screen flex-col text-white fixed  md:flex-1 transition-all md:p-0 px-4 py-8 pt-24 duration-300 z-[100] bg-black w-full top-0 overflow-y-auto md:overflow-visible md:top-auto md:w-auto md:h-auto md:bg-transparent md:static md:z-auto ${
+                isActive ? 'right-0' : '-right-[100vw]'
+              }`}
+            >
+              <button
+                onClick={closeMenu}
+                className="md:hidden absolute top-4 right-4 z-10 h-[2.025rem] w-[1.881rem] text-lightgreen-100 hover:text-lightgreen-100 rounded-[.115rem] bg-darkolivegreen-200 flex flex-col items-center justify-center transition-all duration-300 hover:shadow-[1.8px_1.8px_1.84px_1.4px_rgba(0,_0,_0,_0.91)_inset]"
+              >
+                X
+              </button>
+              <div className="items-center gap-[4.875rem] flex md:flex-row flex-col">
+                <nav className="whitespace-nowrap text-[1.25rem] text-lightgreen-100 font-ocr-x-trial md:mx-auto">
+                  <ul className="flex md:flex-row flex-col items-center gap-8">
+                    <li>
+                      <button
+                        className={clsx(
+                          'text-lightgreen-100 hover:opacity-80',
+                          openHowItWorksModal && 'line-through select-none',
+                        )}
+                        onClick={() => {
+                          setOpenHowItWorksModal(!openHowItWorksModal)
+                          closeMenu()
+                        }}
+                      >
+                        [HOW IT WORKS]
+                      </button>
+                    </li>
+                    <li>
                       <Link
-                        to={link.path}
-                        className={`${location.pathname === link.path ? 'line-through pointer-events-none' : ''
-                          }`}
+                        to="/about"
+                        className={`text-lightgreen-100 hover:opacity-80 ${location.pathname === '/about' ? 'line-through pointer-events-none' : ''}`}
                         onClick={closeMenu}
                       >
-                        {link.label}
+                        [ABOUT]
                       </Link>
                     </li>
-                  ))}
-                </ul>
-              </nav>
-              <Button className="!w-auto">
-                <w3m-button />
-                {/* CONNECT WALLET */}
-              </Button>
+                    <li>
+                      <button
+                        className={clsx(
+                          'text-lightgreen-100 hover:opacity-80',
+                          openFeaturesModal && 'line-through select-none',
+                        )}
+                        onClick={() => {
+                          setOpenFeaturesModal(!openFeaturesModal)
+                          closeMenu()
+                        }}
+                      >
+                        [FEATURES]
+                      </button>
+                    </li>
+                    <li>
+                      <Link
+                        to="/bridge"
+                        className={`text-lightgreen-100 hover:opacity-80 ${location.pathname === '/bridge' ? 'line-through pointer-events-none' : ''}`}
+                        onClick={closeMenu}
+                      >
+                        [BRIDGE]
+                      </Link>
+                    </li>
+                    <li>
+                      <button
+                        className={clsx(
+                          'text-lightgreen-100 hover:opacity-80',
+                          openRoadmapModal && 'line-through select-none',
+                        )}
+                        onClick={() => {
+                          setOpenRoadmapModal(!openRoadmapModal)
+                          closeMenu()
+                        }}
+                      >
+                        [ROADMAP]
+                      </button>
+                    </li>
+                    <li>
+                      <Link
+                        to="/faq"
+                        className={`text-lightgreen-100 hover:opacity-80 ${location.pathname === '/faq' ? 'line-through pointer-events-none' : ''}`}
+                        onClick={closeMenu}
+                      >
+                        [FAQ]
+                      </Link>
+                    </li>
+                  </ul>
+                </nav>
+                <Button className="!w-auto">
+                  <w3m-button />
+                </Button>
+              </div>
             </div>
           </div>
         </div>
-      </div>
-    </header>
+      </header>
+
+      {/* Modals */}
+      <MyModal
+        label={'HOW IT WORKS'}
+        width="39.375rem"
+        open={openHowItWorksModal}
+        handleClose={() => setOpenHowItWorksModal(false)}
+      >
+        <HowItWorks />
+      </MyModal>
+      <MyModal
+        label={'CONNECT WALLET'}
+        width="21.4375rem"
+        open={openConnectWalletModal}
+        handleClose={() => setOpenConnectWalletModal(false)}
+      >
+        <ConnectWallet />
+      </MyModal>
+      <MyModal
+        label={'ROADMAP'}
+        width="41.375rem"
+        open={openRoadmapModal}
+        handleClose={() => setOpenRoadmapModal(false)}
+      >
+        <Roadmap />
+      </MyModal>
+      <MyModal
+        label={'KEY FEATURES'}
+        width="50rem"
+        open={openFeaturesModal}
+        handleClose={() => setOpenFeaturesModal(false)}
+      >
+        <Features />
+      </MyModal>
+    </>
   )
 }
 
--- frontend/src/UI/components/modal/MyModal.tsx
+++ frontend/src/UI/components/modal/MyModal.tsx
@@ -2,48 +2,74 @@ import React, { FC, PropsWithChildren } from 'react'
 
 import clsx from 'clsx'
 import Modal from '@mui/material/Modal'
-import Slide from '@mui/material/Slide/Slide'
+import { Fade } from '@mui/material'
+
+import Draggable from 'react-draggable'
 
 export interface IMyModal {
   open: boolean
   handleClose: () => void
   width?: string
   className?: string
+  label: string
   isReversed?: boolean
 }
 
 const MyModal: FC<PropsWithChildren<IMyModal>> = ({
   open,
   handleClose,
-  width = 'w-full',
+  width = 'w-[26.75rem]',
   children,
   className,
-  isReversed,
+  label,
 }) => {
   return (
-    <Modal
-      aria-labelledby="transition-modal-title"
-      aria-describedby="transition-modal-description"
-      open={open}
-      onClose={handleClose}
-      disableAutoFocus
-      closeAfterTransition
-    >
-      <Slide direction={isReversed ? 'down' : 'up'} in={open}>
-        <div
-          className={clsx(
-            'absolute left-0 p-4 py-6 bg-gray-800  shadow-main flex-col flex',
-            isReversed
-              ? 'top-0 border-b border-blue-400 rounded-bl-[2rem] rounded-br-[2rem]'
-              : 'bottom-0 border-t border-blue-400 rounded-tl-[2rem] rounded-tr-[2rem]',
-            width,
-            className,
-          )}
-        >
-          <div className="relative">{children}</div>
-        </div>
-      </Slide>
-    </Modal>
+    <Draggable handle=".modal-header">
+      <Modal
+        aria-labelledby="transition-modal-title"
+        aria-describedby="transition-modal-description"
+        open={open}
+        onClose={handleClose}
+        closeAfterTransition
+        hideBackdrop={true}
+        disableEnforceFocus
+        disableAutoFocus
+        disableRestoreFocus
+        disableScrollLock={true}
+      >
+        <Fade in={open}>
+          <div
+            className={clsx(
+              'absolute md:top-1/2 md:left-1/2 md:-translate-x-1/2 md:-translate-y-1/2 p-0 bg-transparent border-0 top-0 left-0  md:!h-auto h-full',
+              width,
+              className,
+            )}
+          >
+            <div className="relative md:h-auto h-full">
+              <div className="w-full flex flex-col gap-[0.031rem] md:h-auto h-full">
+                <div className="self-stretch rounded-[.115rem] bg-forestgreen flex flex-col  py-[0.187rem] px-[0.125rem] modal-header">
+                  <div className="self-stretch shadow-[-1.8px_-0.9px_3.69px_rgba(215,_215,_215,_0.18)_inset,_1.8px_1.8px_1.84px_rgba(0,_0,_0,_0.91)_inset] rounded-[.115rem] bg-darkolivegreen-200 flex flex-row items-center justify-between py-[0.25rem] pl-[0.875rem] pr-[0.25rem] gap-[1.25rem] mq450:flex-wrap">
+                    <div className="text-lightgreen-100 font-ocr-x-trial uppercase text-[1.25rem]">
+                      {label}
+                    </div>
+
+                    <button
+                      onClick={handleClose}
+                      className="h-[2.025rem] w-[1.881rem] text-lightgreen-100 hover:text-lightgreen-100 shadow-[1.8px_1.8px_1.84px_rgba(0,_0,_0,_0.91)_inset] rounded-[.115rem] bg-darkolivegreen-200 flex flex-col items-center justify-center transition-all duration-300 hover:shadow-[1.8px_1.8px_1.84px_1.4px_rgba(0,_0,_0,_0.91)_inset]"
+                    >
+                      X
+                    </button>
+                  </div>
+                </div>
+                <div className="self-stretch -mt-px flex flex-col text-[1rem] text-white overflow-y-auto scroller md:h-auto h-full ">
+                  {children}
+                </div>
+              </div>
+            </div>
+          </div>
+        </Fade>
+      </Modal>
+    </Draggable>
   )
 }
 
