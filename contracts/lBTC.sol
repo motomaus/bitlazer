38184973476b77ffaa7c62dc370805e870641335
oldsnapper <oldsnapper@proton.me>
2024-11-12 19:10:46 +0000

    fix: downgrade lBTC to single WBTC support

--- contracts/lBTC.sol
+++ contracts/lBTC.sol
@@ -15,25 +15,18 @@ import "@openzeppelin/contracts-upgradeable/security/ReentrancyGuardUpgradeable.
 contract ZBTC is ReentrancyGuardUpgradeable, AccessControlUpgradeable, ERC20Upgradeable {
     using SafeERC20 for IERC20;
 
-    mapping(address => IERC20) public supportedWrappers;
-    mapping(address => mapping(address => uint256)) internal holderBalances;
-
     bool public paused;
     bool public pausedExtraHolderBalance;
 
-    uint256 public totalBridgeIn;
-    uint256 public totalBridgeOut;
-
     IERC20 public sZBTC;
+    IERC20 public WBTC;
 
     function initialize(string memory name, string memory symbol, address _owner) public initializer {
         require(_owner != address(0), "Owner cannot be 0 address");
         __ERC20_init(name, symbol);
         __AccessControl_init();
         _grantRole(DEFAULT_ADMIN_ROLE, _owner);
-
         __ReentrancyGuard_init();
-
     }
 
     /// @custom:oz-upgrades-unsafe-allow constructor
@@ -47,20 +40,16 @@ contract ZBTC is ReentrancyGuardUpgradeable, AccessControlUpgradeable, ERC20Upgr
         _;
     }
 
-    function setWBTCAddress(address _WBTC) public onlyOwner {
-        supportedWrappers[_WBTC] = IERC20(_WBTC);
-    }
-
-    function removeWBTCAddress(address _WBTC) public onlyOwner {
-        delete supportedWrappers[_WBTC];
-    }
-
     function pause() public onlyOwner {
         paused = true;
     }
 
     function unpause() public onlyOwner {
         paused = false;
+    }
+
+    function setWBTCAddress(address _WBTC) public onlyOwner {
+        WBTC = IERC20(_WBTC);
     }
 
     function pauseExtraHolderBalance() public onlyOwner {
@@ -75,48 +64,32 @@ contract ZBTC is ReentrancyGuardUpgradeable, AccessControlUpgradeable, ERC20Upgr
         sZBTC = IERC20(_sZBTC);
     }
 
-    function getWBTCAddress(address _WBTC) public view returns (address) {
-        return address(supportedWrappers[_WBTC]);
+    function getWBTCAddress() public view returns (address) {
+        return address(WBTC);
     }
 
-    function mint(uint256 amount, address _WBTC) public nonReentrant {
+    function mint(uint256 amount) public nonReentrant {
         require(!paused, "Contract paused");
-        IERC20 WBTC = supportedWrappers[_WBTC];
-        require(WBTC != IERC20(address(0)), "Wrapper not supported");
-        holderBalances[msg.sender][_WBTC] += amount;
         _mint(msg.sender, amount);
         WBTC.safeTransferFrom(msg.sender, address(this), amount);
-        totalBridgeIn += amount;
     }
 
     // If sufficient balance of sZBTC is not available, this function will fail
     // Otherwise, add the extra balance to the holder's balance in order to allow the extra stake accumulated to l3 withdrawals
-    function addExtraHolderBalance(address _holder, uint256 amount, IERC20 _WBTC) public nonReentrant {
+    function addExtraHolderBalance(address _holder, uint256 amount) public nonReentrant {
         require(!pausedExtraHolderBalance, "Extra holder balance paused");
         uint256 sZBTCBalance = sZBTC.balanceOf(_holder);
         require(sZBTCBalance >= amount, "Insufficient sZBTC balance");
         // Verify _WBTC is a supported wrapper
-        require(supportedWrappers[address(_WBTC)] != IERC20(address(0)), "Wrapper not supported");
-        holderBalances[_holder][address(_WBTC)] += amount;
         // Burn the sZBTC
-        sZBTC.transferFrom(_holder, address(1), amount);
+        sZBTC.safeTransferFrom(_holder, address(0x000000000000000000000000000000000000dEaD), amount);
         // Mint the lBTC to the holder
         _mint(_holder, amount);
     }
 
-    function burn(uint256 amount, address _WBTC) public nonReentrant {
+    function burn(uint256 amount) public nonReentrant {
         require(!paused, "Contract paused");
-        IERC20 WBTC = supportedWrappers[_WBTC];
-        require(WBTC != IERC20(address(0)), "Wrapper not supported");
-        require(holderBalances[msg.sender][_WBTC] >= amount, "Insufficient balance in the wrapper holding");
-        // Ensured that the user has enough balance in the wrapper holding so shouldn't fail
-        holderBalances[msg.sender][_WBTC] -= amount;
         _burn(msg.sender, amount);
         WBTC.transfer(msg.sender, amount);
-        totalBridgeOut += amount;
-    }
-
-    function getHolderBalance(address _holder, address _WBTC) public view returns (uint256) {
-        return holderBalances[_holder][_WBTC];
     }
 }
